73681de28810744f0daff941df0e5b4a95afffdd
murodil01 <nurmamatovmirodil978@gmail.com>
2025-05-07 14:35:17 +0500

    ulandi

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,47 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import Navbar from './components/Navbar';
+import Brand from './components/Brand';
+import Cards from './components/Cards';
+import Slider from './components/Slider';
+import Statistic from './components/Statistic';
+import Footer from './components/Footer';
+
+function App() {
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (product) => {
+    setCart((prev) => [...prev, { ...product, quantity: 1 }]);
+  };
+
+  const removeFromCart = (id) => {
+    setCart((prev) => prev.filter(item => item.id !== id));
+  };
+
+  const updateQuantity = (id, quantity) => {
+    setCart((prev) => prev.map(item => item.id === id ? { ...item, quantity } : item));
+  };
+
+  return (
+    <>
+      <Navbar />
+      <Brand cart={cart} />
+      <Slider />
+      <Cards addToCart={addToCart} />
+      <Statistic />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
+
+
+
+/*import { useState } from 'react'
 import Navbar from "./components/Navbar"
 import Brand from './components/Brand'
-import Slider from './components/Slider'
 import Cards from "./components/Cards"
+import Slider from './components/Slider'
 import Statistic from './components/Statistic'
 import Footer from "./components/Footer"
 function App() {
@@ -19,4 +58,4 @@ function App() {
   )
 }
 
-export default App
+export default App*/
